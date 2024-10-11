eca8bf0b996ebc83c12cd3fcef6c0bd3f9471638
Jhon-Develop <asprillajhon73@gmail.com>
2024-10-11 08:30:12 -0500

    Creation of the configuration for migrations

--- DataBase/ApplicationDbContext.cs
+++ DataBase/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using To_Do.Models;
 
 namespace To_Do.DataBase
 {
@@ -10,10 +11,8 @@ namespace To_Do.DataBase
     {
         public ApplicationDbContext(DbContextOptions options) : base(options){ }
 
+        public DbSet<User> Users { get; set; }
+        public DbSet<TaskItem> TaskItems { get; set; }
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            
-        }
     }
 }
